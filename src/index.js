1c7f0d953e0475c965f122c2d4ac958d79204179
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-09-17 13:32:59 +0300

    hw-10
    hello

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+function fetchCountries(name){
+return fetch(()=>{})
+
+}
+
+
+// https://restcountries.com/v3.1/name/{name}
+console.log("hello");
